9a432dbd8e41ff89100fce27a8f525f9b9ad54cd
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2014-11-13 17:57:27 +0100

    Encode strings and add test conditions

--- holonet/core/management/commands/policy_service.py
+++ holonet/core/management/commands/policy_service.py
@@ -1,4 +1,5 @@
 # -*- coding: utf8 -*-
+
 from django.core.management.base import BaseCommand
 from optparse import make_option
 import sys
@@ -8,11 +9,6 @@ import signal
 import time
 
 from django.conf import settings
-
-
-def unique(seq):
-    st = set(seq)
-    return list(st)
 
 
 class ThreadedTCPServer(socketserver.ThreadingMixIn, socketserver.TCPServer):
@@ -27,26 +23,26 @@ class PostfixAccessPolicyHandler(socketserver.BaseRequestHandler):
         """Consider a set of params sent by postfix and
            see if we want to hadle mail for the address"""
 
-        print(params)
-
         if not 'recipient' in params:
-            return {'action': 'reject no recipient listed'}
+            return {'action': 'REJECT No recipient listed'}
+
         recipient = params["recipient"]
         splitted = recipient.split('@')
+
         if len(splitted) < 2:
             return {'action': 'REJECT Address incomplete'}
+
         prefix, domain = splitted
-        if prefix in ('restricted', ):
-            return {'action': 'DUNNO'}
 
-        elif domain in settings.MASTER_DOMAINS:
+        if domain in settings.MASTER_DOMAINS:
 
-            if ['test', 'eirik']:
+            if prefix == 'eirik':
                 return {'action': 'DUNNO'}
             else:
                 return {'action': 'REJECT Address does not exist'}
+
         else:
-                return {'action': 'REJECT Domain not handled by holonet'}
+                return {'action': 'REJECT Domain is not handled by holonet'}
 
     def handle(self):
         """
@@ -55,21 +51,24 @@ class PostfixAccessPolicyHandler(socketserver.BaseRequestHandler):
         """
         # self.request is the TCP socket connected to the client
         data_list = []
-        packet = self.request.recv(1024).split("\n")
+        packet = self.request.recv(1024).decode('utf-8').split("\n")
 
         while ''.join(packet).strip():
             data_list.extend(packet)
             if data_list and not data_list[-1]:
                 break
-            packet = self.request.recv(1024).split("\n")
+            packet = self.request.recv(1024).decode('utf-8').split("\n")
 
         data = filter(lambda line: len(line), data_list)
         if not data:
             return
+        
         params = dict([line.split("=", 1) for line in data])
         response = self.consider(params)
-        self.request.sendall("\n".join(['='.join(item) for item
-                                        in response.iteritems()]+['\n']))
+
+        self.request.sendall(
+            "\n".join(['='.join(item) for item in response.items()] + ['\n']).encode('utf-8')
+        )
 
 
 class Command(BaseCommand):
