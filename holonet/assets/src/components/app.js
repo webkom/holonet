934ba01bb8f4262ee0320cf9c355362ef902f6c2
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-05-18 16:24:14 +0200

    Add navbar

--- holonet/assets/src/components/app.js
+++ holonet/assets/src/components/app.js
@@ -1,10 +1,24 @@
 import React from 'react';
 
+import Navbar from './navbar/navbar';
+
 export default class App extends React.Component {
+
+  navbarLinks() {
+    return {
+      left: [],
+      right: [
+        {text: 'API', url: '/api'},
+        {text: 'Logout', url: '/logout'}
+      ]
+    };
+  }
 
   render() {
     return (
-      <h1>Holonet - Under development</h1>
+      <div className='container'>
+        <Navbar links={this.navbarLinks()} />
+      </div>
     );
   }
 
