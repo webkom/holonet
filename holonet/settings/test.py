5c3c90c9c26a2dbe6eba9a1ebb49f1a24cbc251f
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-08-31 06:42:10 +0200

    Remove unused test setting

--- holonet/settings/test.py
+++ holonet/settings/test.py
@@ -7,14 +7,12 @@ TEST_RUNNER = "djcelery.contrib.test_runner.CeleryTestSuiteRunner"
 SENDER_WHITELIST_ENABLED = False
 DOMAIN_WHITELIST_ENABLED = False
 
-STATICFILES_STORAGE = 'pipeline.storage.PipelineStorage'
-
 SECRET_KEY = 'test'
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'HOST': '127.0.0.1',
-        'NAME': 'hansolo'
+        'NAME': 'holonet'
     }
 }
