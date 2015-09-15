bdd7c4ee9802427f4b551776aa034e1ab92d6797
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-09-15 23:29:24 +0200

    Fix build failure related to the sendmail backend

--- holonet/backends/test/test_sendmail.py
+++ holonet/backends/test/test_sendmail.py
@@ -1,4 +1,5 @@
 import mock
+from django.conf import settings
 from django.core.mail import EmailMessage
 from django.test import TestCase
 
@@ -12,8 +13,9 @@ popen.stdin.close = mock.Mock()
 
 class SendmailTestCase(TestCase):
 
+    @mock.patch('os.path.isfile', return_value=True)
     @mock.patch('subprocess.Popen', return_value=popen)
-    def test_sendmail(self, mock_popen):
+    def test_sendmail(self, mock_popen, mock_isfile):
         email = EmailMessage('Hello', 'Body goes here', 'from@example.com',
                              ['to@example.com'], [])
 
@@ -21,8 +23,9 @@ class SendmailTestCase(TestCase):
         backend.send_messages([email])
 
         mock_popen.assert_called_once_with(
-            ['/usr/sbin/sendmail', '-G', '-i', '-f', 'from@example.com', 'to@example.com'],
+            [settings.SENDMAIL_EXECUTABLE, '-G', '-i', '-f', 'from@example.com', 'to@example.com'],
             close_fds=True, stdin=-1)
+        mock_isfile.assert_called_once_with(settings.SENDMAIL_EXECUTABLE)
 
         first_write_call = popen.stdin.write.call_args[0]
         message_bytes = first_write_call[0]
