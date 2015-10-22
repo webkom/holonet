76049f2dd002279981bf5df479a22d7c13035ff0
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-10-22 04:12:32 +0200

    Fix lint

--- holonet/frontend/components/wrapper.js
+++ holonet/frontend/components/wrapper.js
@@ -3,7 +3,8 @@ import { Component, PropTypes } from 'react';
 export default class Wrapper extends Component {
 
   static propTypes = {
-    dispatch: PropTypes.func.isRequired
+    dispatch: PropTypes.func.isRequired,
+    children: PropTypes.object.isRequired
   };
 
   render() {
