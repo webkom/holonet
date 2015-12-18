063c08bd23f14b099b8e92227b78d1d092ec3b9f
Eirik Martiniussen Sylliaas <eirik@sylliaas.no>
2015-12-19 00:43:13 +0100

    Install django-environment for production use

--- holonet/settings/production.py
+++ holonet/settings/production.py
@@ -1,6 +1,24 @@
 import os
 
+import environ
+
 from holonet.settings import BASE_DIR
+
+root = environ.Path(__file__) - 3
+env = environ.Env(DEBUG=(bool, False),)
+environ.Env.read_env()
+
+DEBUG = env('DEBUG')
+
+DATABASES = {
+    'default': env.db()
+}
+
+CACHES = {
+    'default': env.cache(),
+}
+
+SECRET_KEY = env('SECRET_KEY')
 
 WEBPACK_LOADER = {
     'DEFAULT': {
